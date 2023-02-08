f6849614467e6f6b1a91b0cb84943b4ffe3bf0d3
wellington <wrogeriowrs@gmail.com>
2023-02-08 10:09:15 -0300

    data no titulo

--- HouseControl.UI/wwwroot/js/jsDash.js
+++ HouseControl.UI/wwwroot/js/jsDash.js
@@ -15,6 +15,9 @@ function loadData() {
         dataType: "json",
         success: function (data) {
             var dataList = data.dados;
+            var mesAno = document.getElementById("MesAno").value;
+            var mesNome = traduzMesNome(mesAno.split("-")[1]).toUpperCase();
+            var ano = mesAno.split("-")[0];
 
             // order by porcentagem desc
             _dataList = dataList.sort(function (a, b) {
@@ -22,7 +25,7 @@ function loadData() {
             });
 
             $("#rowDash").empty();
-            $("#gastoTotal").text(data.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }))
+            $("#gastoTotal").text(mesNome + "/" + ano + " " + data.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }))
 
             var _html = _dataList.map(x => {
                 return `
@@ -45,4 +48,51 @@ function loadData() {
             console.log(errormessage);
         }
     })
+}
+
+function traduzMesNome(mes) {
+    var nomeMes = ""
+    switch (mes) {
+        case '01':
+            nomeMes = "Janeiro";
+            break;
+        case '02':
+            nomeMes = "Fevereiro";
+            break;
+        case '03':
+            nomeMes = "Março";
+            break;
+        case '04':
+            nomeMes = "Abril";
+            break;
+        case '05':
+            nomeMes = "Maio";
+            break;
+        case '06':
+            nomeMes = "Junho";
+            break;
+        case '07':
+            nomeMes = "Julho";
+            break;
+        case '08':
+            nomeMes = "Agosto";
+            break;
+        case '09':
+            nomeMes = "Setembro";
+            break;
+        case '10':
+            nomeMes = "Outubro";
+            break;
+        case '11':
+            nomeMes = "Novembro";
+            break;
+        case '12':
+            nomeMes = "Dezembro";
+            break;
+        default:
+            nomeMes = "Janeiro";
+            break;
+    }
+
+    return nomeMes;
 }
