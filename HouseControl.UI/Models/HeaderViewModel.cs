bc5e697051cb48c8af9bc7cf7251ee4a1f07cabe
wellington <wrogeriowrs@gmail.com>
2023-02-16 20:46:20 -0300

    acceskey

--- HouseControl.UI/Models/HeaderViewModel.cs
+++ HouseControl.UI/Models/HeaderViewModel.cs
@@ -8,4 +8,5 @@ public class HeaderViewModel
     public string ButtonIcon { get; set; }
     public string ControllerToGo { get; set; }
     public string ActionToGo { get; set; }
+    public string AccessKey { get; set; }
 }
